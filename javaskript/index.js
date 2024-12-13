f1297b8d1defbe437b12fe04dae453105f29128c
lostojic <lostojic@unisb.hr>
2024-12-13 13:24:12 +0100

    danas

---
+++ javaskript/index.js
@@ -0,0 +1,22 @@
+/*  var broj1 = prompt("unesi prvi broj:" );
+var broj2 = prompt("unesi drugi broj:");
+
+if(broj1>broj2)
+{
+    alert(broj1 + "je veci od broja " + broj2);
+}
+else if(broj1 < broj2)
+{
+   alert(broj2 + "je veci od broja " + broj1) ;
+}
+else{
+    alert("brojevi su jednaki");
+}  */
+
+    //love calculator
+    var ime1 = prompt("unesi svoje ime");
+    var ime2 = prompt("unesi ime simpatije");
+
+    var n = Math.floor(Math.random()*100)+1; //1-100%
+
+    alert("kompatibilni ste: " +  n +  "%" )
